89d800759dafbbeadc259870d10fa7805e5ad5cb
Lahorde <mr.lahorde@laposte.net>
2016-09-07 11:27:41 +0200

    Clean CafeSourire folder

--- gateway_software/examples/CafeSourire/data/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/gome/Bureau/Projet_spe/opencv/data
-# Build directory: /home/gome/Bureau/Projet_spe/opencv/data
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
