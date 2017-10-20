26e5d4f67c957f2904415a0afc56cb3bb85939af
Lahorde <mr.lahorde@laposte.net>
2017-10-20 16:48:12 +0200

    Modif for openhab2 + clean in sounds

--- gateway_software/examples/CafeSourire/openhab_config/helper_scripts/play_sound.sh
+++ gateway_software/examples/CafeSourire/openhab_config/helper_scripts/play_sound.sh
@@ -5,9 +5,17 @@ curr_folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 # Use sox to play music : http://sox.sourceforge.net/sox.html
 if [[ $2 == '-f' ]]
 then
-  play $1 fade t 4 -0 4  
+  if ! play $1 fade t 4 -0 4  
+  then
+    echo "Cannot play sound $1"
+    exit 1
+  fi
 else
-  play $1 
+  if ! play $1 
+  then
+    echo "Cannot play sound $1"
+    exit 1
+  fi
 fi
 
 #Info for openhab
--- gateway_software/examples/CafeSourire/openhab_config/helper_scripts/say.sh
+++ gateway_software/examples/CafeSourire/openhab_config/helper_scripts/say.sh
@@ -20,19 +20,7 @@ then
   exit 1
 fi
 
-curr_folder="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-was_playing=$(source $curr_folder/mpd_play_pause.sh)
-while ! play /tmp/sound_temp.wav > /dev/null 2>&1 
-do
-  sleep 1
-done 
-if [ ! -z "$was_playing" ] && [ "$was_playing" == "was_playing" ]
-then
-  sleep 3 
-  source $curr_folder/mpd_play_pause.sh play
-fi
-
-if [ $? -ne 0 ]
+if ! play /tmp/sound_temp.wav > /dev/null 2>&1 
 then
   #Info for openhab
   echo "Could not say $2 - error : $?"
